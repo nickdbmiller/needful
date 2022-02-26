7c6ba263a9bf70a8c7092e9563123ba1b0dffc3a
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-26 17:50:15 -0500

    Create favorites relationships.

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_many :reviews, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+    has_many :products, :through => :favorites
+
     has_secure_password
 
     validates :username, presence: true, uniqueness: true
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_26_205111) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_26_224706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_favorites_on_product_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -45,6 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_26_205111) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "products"
+  add_foreign_key "favorites", "users"
   add_foreign_key "reviews", "products"
   add_foreign_key "reviews", "users"
 end
