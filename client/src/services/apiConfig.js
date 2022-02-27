cac40942f95b67d8f380bb1aad1ecadef5390f68
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-27 16:42:31 -0500

    Creates axios instance for development and production.

---
+++ client/src/services/apiConfig.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+// Different URL's for development and deployed app.
+const apiBases = {
+    development: 'http://localhost:3000',
+    production: 'https://needfulapi.herokuapp.com/api'
+}
+
+const baseUrl = window.location.hostname === 'localhost' ? apiBases.development : apiBases.production
+
+export const api = axios.create({
+    baseURL: baseUrl
+})
