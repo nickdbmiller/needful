36562f9c7e13ac5d62c93c3f05b3bb177e3d9937
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-27 17:28:39 -0500

    Add auth, get user favorites, set user and product id on create favorite.

--- server/app/controllers/favorites_controller.rb
+++ server/app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < ApplicationController
   before_action :set_favorite, only: %i[ show update destroy ]
+  before_action :authorize_request, only: [:create, :update, :destroy]
 
   # GET /favorites
   def index
@@ -8,17 +9,25 @@ class FavoritesController < ApplicationController
     render json: @favorites
   end
 
+  # GET /users/:user_id/favorites
+  def get_user_favorites
+    @user = User.find(params[:user_id])
+    render json: @user.favorites
+  end
+  
   # GET /favorites/1
   def show
     render json: @favorite
   end
-
+  
   # POST /favorites
   def create
     @favorite = Favorite.new(favorite_params)
-
+    @favorite.user = @current_user
+    @favorite.product_id = params[:product_id]
+    
     if @favorite.save
-      render json: @favorite, status: :created, location: @favorite
+      render json: @favorite, status: :created
     else
       render json: @favorite.errors, status: :unprocessable_entity
     end
@@ -36,6 +45,7 @@ class FavoritesController < ApplicationController
   # DELETE /favorites/1
   def destroy
     @favorite.destroy
+    render json: @favorite
   end
 
   private
