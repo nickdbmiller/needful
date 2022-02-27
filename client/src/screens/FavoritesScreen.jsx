40802fd349fab057be0ed707d2c0ad9914ec9a8f
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-27 11:41:37 -0500

    Basic styling and layout.

--- client/src/screens/FavoritesScreen.jsx
+++ client/src/screens/FavoritesScreen.jsx
@@ -1,9 +1,23 @@
+import Carousel from "../components/Carousel";
+import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 
 export default function FavoritesScreen() {
     return (
         <Layout>
-            <div>FavoritesScreen</div>
+            <div
+                className="font-noto-display text-2xl text-rose-100 bg-rose-700 p-2 rounded-lg mx-6 my-3"
+            >
+                <Hero
+                    text = "Need anything? Add it here."
+                />
+            </div>
+            <div
+                className="font-noto-display text-2xl text-rose-1000 bg-rose-300 p-2 my-3"
+            >
+                <h3>Products</h3>
+                <Carousel />
+            </div>
         </Layout>
     )
 }
