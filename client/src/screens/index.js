6109e8e175e15fb4de93254e05d64c63681c854a
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-26 21:03:07 -0500

    Index of screen components to reduce import lines in App.js.

---
+++ client/src/screens/index.js
@@ -0,0 +1,6 @@
+// Sets default export for all screens.
+export { default as AuthScreen } from "./AuthScreen";
+export { default as FavoritesScreen } from "./FavoritesScreen";
+export { default as LandingScreen } from "./LandingScreen";
+export { default as ProductDetailScreen } from "./ProductDetailScreen";
+export { default as SearchScreen } from "./SearchScreen";
