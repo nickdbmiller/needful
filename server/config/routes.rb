6c61f73cb1025adcc27076003f0156cd34370234
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-27 14:03:52 -0500

    Nest favorites in users, and reviews in products.

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :favorites
-  resources :reviews
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post '/auth/login', to: 'authentications#login'
   get '/auth/verify', to: 'authentications#verify'
   
-  resources :users
-  resources :products
+  resources :users do
+    resources :favorites
+  end
+  
+  resources :products do
+    resources :reviews
+  end
 end
