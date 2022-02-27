c53a649a9f47e592a45e3d7f345cabd4105be509
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-27 10:49:31 -0500

    Searchbar layout and basic functionaility.

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 export default function Searchbar() {
+    const [input, setInput] = useState("");
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>Searchbar</div>
+        <form
+            id="searchbar"
+            onSubmit={handleSubmit}
+            className="text-md p-2 font-noto-display bg-rose-300 flex justify-evenly text-rose-1000"
+        >
+            <input
+                id="query"
+                name="query"
+                type="text"
+                value={input}
+                placeholder="Search"
+                onChange={(e) => {
+                    setInput(e.target.value)
+                    console.log(e.target.value)
+                }}
+            />
+            <Link
+                to="/search"
+                className="hover:text-rose-100 hover:-translate-y-0.5 transform transition
+                focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-900 focus:ring-opacity-50
+                active:text-rose-200"
+            >
+                <FontAwesomeIcon className="text-2xl text-rose-700" icon={faMagnifyingGlass} />
+            </Link>
+        </form>
     )
 }
