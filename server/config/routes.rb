afdd76e6d2910f145fe67ff6ea35a72c2830b96c
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-27 17:27:22 -0500

    Add route to get user favorites, and all reviews.

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post '/auth/login', to: 'authentications#login'
   get '/auth/verify', to: 'authentications#verify'
-  
+
   resources :users do
     resources :favorites
   end
-  
+
+  get '/users/:user_id/favorites', to: 'favorites#get_user_favorites'
+
   resources :products do
     resources :reviews
   end
+
+  get '/reviews', to: 'reviews#get_all_reviews'
 end
