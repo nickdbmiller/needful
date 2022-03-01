9668d142b29edb29dc119093b3cd6aca8e96b08a
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-28 22:58:58 -0500

    Mapped product info displays to card.

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,14 +1,19 @@
-export default function ProductCard() {
+import { Link } from 'react-router-dom'
+
+export default function ProductCard(props) {
     return (
-        <div
-            className="bg-rose-100 rounded-2xl"
-        >
-            <h4>Product Title</h4>
-            <img
-                src="https://images.squarespace-cdn.com/content/v1/5ad5e405372b9673106e3a9a/1523981232273-NLIFBO08LRSCA3B7ZE80/WLCC-product+range-web_250g.png?format=1000w"
-                alt="Product Image"
-                className="w-40"
-            />
-        </div>
+        <Link to = {`/product/${props.id}`}>
+            <div
+                className="bg-rose-100 rounded-2xl"
+            >
+                <h4>{props.title}</h4>
+                <img
+                    src={props.imgSrc}
+                    alt={props.imgAlt}
+                    className="w-40"
+                />
+                <p>${props.price}</p>
+            </div>
+        </Link>
     )
 }
