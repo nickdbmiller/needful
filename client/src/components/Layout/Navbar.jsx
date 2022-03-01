6e2cd3b7b6f9da5f91ed7badd4014397880a6d7e
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-28 20:40:17 -0500

    Logout button logs the user out and conditionally renders login button if user is logged in.

--- client/src/components/Layout/Navbar.jsx
+++ client/src/components/Layout/Navbar.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouseChimney, faHeart, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { faHouseChimney, faHeart, faArrowRightToBracket, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
         <nav className="text-md px-4 py-4 font-noto-display bg-rose-500">
+
+            {/* Menu */}
             <ul className="flex justify-evenly text-rose-1000">
+
+                {/* Home */}
                 <li
                     className="hover:text-rose-100 hover:-translate-y-0.5 transform transition
                     focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-900 focus:ring-opacity-50
@@ -19,6 +23,8 @@ export default function Navbar() {
                         <h4>home</h4>
                     </Link>
                 </li>
+
+                {/* Favorites */}
                 <li
                     className="hover:text-rose-100 hover:-translate-y-0.5 transform transition
                     focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-900 focus:ring-opacity-50
@@ -32,11 +38,24 @@ export default function Navbar() {
                         <h4>needs</h4>
                     </Link>
                 </li>
+
+                {/* Login/Logout */}
                 <li
                     className="hover:text-rose-100 hover:-translate-y-0.5 transform transition
                     focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-900 focus:ring-opacity-50
                     active:text-rose-200"
                 >
+                    {props.currentUser ? 
+                    // Login Button
+                    <div
+                        className="flex flex-col"
+                        onClick={() => props.logout()}
+                    >
+                        <FontAwesomeIcon className="text-2xl" icon={faArrowRightFromBracket} />
+                        <h4>logout</h4>
+                    </div>
+                    :
+                    // Logout Button
                     <Link
                         className="flex flex-col"
                         to="/auth"
@@ -44,6 +63,7 @@ export default function Navbar() {
                         <FontAwesomeIcon className="text-2xl" icon={faArrowRightToBracket} />
                         <h4>login</h4>
                     </Link>
+                }
                 </li>
             </ul>
         </nav>
