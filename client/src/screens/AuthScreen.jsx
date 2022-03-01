50c71a5662fa3ddb8c12659adcc586921b21401f
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-28 20:41:27 -0500

    Auth form logs the user in and sets the current user to the logged in user.

--- client/src/screens/AuthScreen.jsx
+++ client/src/screens/AuthScreen.jsx
@@ -2,81 +2,82 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import Hero from "../components/Hero";
-import Layout from "../components/Layout/Layout";
+import { loginUser } from "../services/user";
 
-export default function AuthScreen() {
-    const [input, setInput] = useState("");
+export default function AuthScreen(props) {
     const navigate = useNavigate()
 
-    const handleSubmit = (e) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // navigate("/")
+        const loginData = {
+            username,
+            password
+        }
+
+        const res = await loginUser(loginData)
+        props.setCurrentUser(res)
+        navigate("/");
     };
 
     return (
-        <Layout>
-            <section
-                className="flex flex-col items-center font-noto-display"
+        <section
+            className="flex flex-col items-center font-noto-display"
+        >
+            <div
+                className="text-2xl text-rose-100 bg-rose-700 p-2 rounded-2xl mx-6 my-3"
             >
-                <div
-                    className="text-2xl text-rose-100 bg-rose-700 p-2 rounded-2xl mx-6 my-3"
-                >
-                    <Hero
-                        text = "First visit or joyful return, we’re glad you’re here."
-                    />
-                </div>
-                <h3
-                    className="text-4xl mb-2"
-                >
-                    Welcome
-                </h3>
+                <Hero
+                    text = "First visit or joyful return, we’re glad you’re here."
+                />
+            </div>
+            <h3
+                className="text-4xl mb-2"
+            >
+                Welcome
+            </h3>
 
-                {/* Auth Form */}
-                <form
-                    id="auth"
-                    onSubmit={handleSubmit}
-                    className="text-lg p-2 font-noto-display bg-rose-500 flex flex-col items-center text-rose-1000
-                    rounded-2xl"
+            {/* Auth Form */}
+            <form
+                id="auth"
+                onSubmit={handleSubmit}
+                className="text-lg p-2 font-noto-display bg-rose-500 flex flex-col items-center text-rose-1000
+                rounded-2xl"
+            >
+                {/* Username */}
+                <label className="block" htmlFor="username">username</label>
+                <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className="text-lg font-noto-display bg-rose-100 flex text-rose-1000 rounded-2xl pl-2"
+                />
+
+                {/* Password */}
+                <label className="block mt-6" htmlFor="password">password</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="text-lg font-noto-display bg-rose-100 flex text-rose-1000 rounded-2xl pl-2"
+                />
+
+                {/* Submit Button */}
+                <button
+                    className="bg-rose-1000 text-rose-700 hover:text-rose-100
+                    hover:-translate-y-0.5 transform transition
+                    focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-900 focus:ring-opacity-50
+                    active:text-rose-200 rounded-2xl py-2 px-6 my-6"
                 >
-                    {/* Email */}
-                    <label className="block" htmlFor="email">email</label>
-                    <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        value={input}
-                        onChange={(e) => {
-                            setInput(e.target.value)
-                            console.log(e.target.value)
-                        }}
-                        className="text-lg font-noto-display bg-rose-100 flex text-rose-1000 rounded-2xl pl-2"
-                    />
-
-                    {/* Password */}
-                    <label className="block mt-6" htmlFor="password">password</label>
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        value={input}
-                        onChange={(e) => {
-                            setInput(e.target.value)
-                            console.log(e.target.value)
-                        }}
-                        className="text-lg font-noto-display bg-rose-100 flex text-rose-1000 rounded-2xl pl-2"
-                    />
-
-                    {/* Submit Button */}
-                    <button
-                        className="bg-rose-1000 text-rose-700 hover:text-rose-100
-                        hover:-translate-y-0.5 transform transition
-                        focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-900 focus:ring-opacity-50
-                        active:text-rose-200 rounded-2xl py-2 px-6 my-6"
-                    >
-                        enter
-                    </button>
-                </form>
-            </section>
-        </Layout>
+                    enter
+                </button>
+            </form>
+        </section>
     )
 }
