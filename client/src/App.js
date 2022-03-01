9d2fec3857a6bbf819f03b7efefc3df62a66ccbc
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-28 20:38:08 -0500

    User state lives on App.js. Logout() and the current user are passed to layout as props.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,42 @@
-import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import './App.css';
 
+import { verifyUser } from './services/user.js';
 import { AuthScreen, FavoritesScreen, LandingScreen, ProductDetailScreen, SearchScreen } from './screens';
+import Layout from './components/Layout/Layout';
 
 function App() {
+  const navigate = useNavigate()
+
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    const getUser = async () => {
+      const user = await verifyUser()
+      setCurrentUser(user)
+    }
+    getUser()
+  }, [])
+
+  const logout = () => {
+    localStorage.removeItem('authToken')
+    setCurrentUser(null)
+    navigate("/");
+  }
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<LandingScreen />} />
-        <Route path="/auth" element={<AuthScreen />} />
-        <Route path="/favorites" element={<FavoritesScreen />} />
-        <Route path="/product" element={<ProductDetailScreen />} />
-        <Route path="/search" element={<SearchScreen />} />
-      </Routes>
+      <Layout currentUser={currentUser} logout={logout}>
+        <Routes>
+          <Route path="/" element={<LandingScreen />} />
+          <Route path="/auth" element={<AuthScreen setCurrentUser={setCurrentUser}/>} />
+          <Route path="/favorites" element={<FavoritesScreen />} />
+          <Route path="/product" element={<ProductDetailScreen />} />
+          <Route path="/search" element={<SearchScreen />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
